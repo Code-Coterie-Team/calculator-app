5c9fef530a2fe265b26aa82e98231ae77ab344ee
Arezoo <arezooayoblo@gmail.com>
2024-10-11 10:39:47 +0330

    change the path

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const showVar=document.getElementById('result');
 
+    const showVar=document.getElementById('result');
 
 function getValue(val){
     document.getElementById('result').value+=val;
@@ -16,43 +16,60 @@ function del(){
 function calculate(){
     const display=document.getElementById('result');
     const experssion=display.value;
-    try{
-        const result=handelExpression(experssion);
-        display.value=result;
-    } catch(erorr){
-        display.value='Error'
-    };
+    const showResult=handelExpression(experssion);
     
+    display.value=showResult;
 }
-function  handelExpression(exp){
-    const numbers=exp.split(/[\+\-\x\/][-]/).map(Number);
-    const operators=exp.split(/[0-9]+/).filter(Boolean);
-    const firstResult=operators.reduce((acc,operator,index)=>{
-        if(operator==="x"){
-            
-            return acc * numbers[index+1];
-            
-        }  else if(operator==="/") {
-            return acc/numbers[index+1]
-        }
-        else{
-            return acc;
-        }
-    },numbers[0]);
+function handelExpression(exp) {
+    const array = exp.match(/(\d+|\x|\+|\-|\s+|\/)/g).map(item => item.trim())
+                    .filter(item => item);
 
-    const finalResult=operators.reduce((acc,operator,index)=>{
-        if(operator==="+"){
-            return acc + numbers[index+1];
+    
+    return evaluateExpression(array); 
+}
+
+function evaluateExpression(array){
+    const len=array.length;
+    while(len > 1 ){
+        const indexMultiply=array.indexOf('x');
+        const indexDivide=array.indexOf('/');
+        if(indexMultiply !== -1 &&(indexDivide === -1|| indexMultiply<indexDivide)){
+            const leftOperand = Number(array[indexMultiply- 1]);
+            const rightOperand = Number(array[indexMultiply + 1]);
+            const result= leftOperand * rightOperand;
             
-        } else if (operator==="-"){
-            return acc - numbers[index-1];
+            array.splice(indexMultiply-1,3,result);
+            continue;
         }
-        else{
-            return acc
-        };
+        if (indexDivide !== -1){
+            const leftOperand = Number(array[indexDivide- 1]);
+            const rightOperand = Number(array[indexDivide + 1]);
+            const result=leftOperand/rightOperand;
+            array.splice(indexDivide-1,3,result);
+            
+            continue;
+        }
+        const indexAdd=array.indexOf('+');
+        if(indexAdd !== -1){
+            const leftOperand = Number(array[indexAdd- 1]);
+            const rightOperand = Number(array[indexAdd + 1]);
+            const result = leftOperand + rightOperand;
+            
+            array.splice(indexAdd-1,3,result);
+            console.log(result);
+            continue;
+        }
+        const indexSub=array.indexOf('-');
+        if(indexSub !== -1){
+            const leftOperand = Number(array[indexSub- 1]);
+            const rightOperand = Number(array[indexSub + 1]);
+            const result=leftOperand-rightOperand;
+            array.splice(indexSub-1,3,result);
+            
+            continue;
+        }
+        console.log(array[0])
+        return array[0];
         
-    }, firstResult);
-    console.log(finalResult);
-    return finalResult;
-   
+    }
 }
